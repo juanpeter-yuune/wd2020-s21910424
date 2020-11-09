20f4f7b5c22c299bef9de07198bd99ff112af600
juanpeter-yuune <juanpetert77@gmail.com>
2020-11-09 21:27:42 +0800

    java script dalam html

---
+++ Pertemuan_23/script.js
@@ -0,0 +1,47 @@
+//variabel
+
+//let & cons
+
+// let _umur = 25;
+// _umur = 26;
+// // let nilai = 95.5;
+// // let sudahMenikah = false;
+// // let $nama = "John Doe";
+// // console.log(_umur);
+// // console.log($nama);
+
+//     let alamat;
+//     console.log(alamat);
+
+// // const tahunLahir = 2010;
+// // console.log(tahunLahir);
+
+// let angka1 = 10;
+// let angka2 = 20;
+// console.log(angka1+angka2);
+
+// let umur = 30;
+// let namaDepan = "John";
+// let namaBelakang = "Doe";
+// console.log("Nama Saya" + namaDepan + " " namaBelakang " " + umur " ");
+
+//ternary operator
+// let angka1 = 10;
+// let angka2 = 9;
+
+// console.log(angka1 <= angka2);
+
+// //operator logika
+// let kondisi1 = 1<2;
+// let kondisi2 = true;
+
+// console.log(kondisi1 || kondisi2);
+// console.log(!log)
+
+//let angka = prompt("Masukan angka :");
+//console.log("angka yang dimasukan adalah " + angka);
+
+let berat=prompt ("Masukan berat :") ;
+let tinggi=prompt ("Masukan tinggi :");
+let bmi=berat/(tinggi*tinggi);
+console.log("BMI =" + bmi );
